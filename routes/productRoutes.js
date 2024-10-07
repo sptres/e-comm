c58d6cea03d0e132541481ac9950474aefeef7d9
Shawn Park <sptres@Shawns-MacBook-Pro-2.local>
2024-10-07 10:37:41 -0700

    debug

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 const { isAuthenticated } = require('../middleware/auth');
 
-router.get('/api', productController.getProducts); // Changed this line
+router.get('/api', productController.getProducts);
 router.get('/brands', productController.getBrands);
 router.get('/api/details/:productId', productController.getProductDetails);
 router.post(
